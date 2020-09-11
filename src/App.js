519abac1d8800de7cf58841450fba2ff5165ca5e
carolstran <carolynstransky@gmail.com>
2020-09-11 17:36:04 +0200

    :woman_artist: Write one potential answer

--- src/App.js
+++ src/App.js
@@ -4,29 +4,46 @@ import logo from "./logo.svg";
 import team from "./team";
 
 /* 
-Use your new JSX skills to recreate our team chart.
+Here's a potential solution to the homework.
 
-This app should have:
-- The same header as the original.
-- A table showing everyone's photo, name, and role (animals is a bonus).
+Keep in mind that this is only ONE WAY to do it.
+Your JSX might look different - and that's ok!
 
-All of the imports have been provided for you, including:
-- Table data (team.js)
-- CSS (styles.css)
-- React logo (logo.svg)
-
-In your JSX, you'll need to use each of the following at least once...
-- className
-- variable
-- variable attribute
-- self-closing tag
-- map
-- key
-
-Feel free to have fun and experiment with others too!
-
-If you get stuck or need help, please reach out on Slack.
+If you have questions, please reach out on Slack.
 */
 export default function App() {
-  return <div>Replace this with your beautiful JSX</div>;
+  return (
+    <>
+      <header className="App-header">
+        <img 
+          src={logo} 
+          alt="Spinning React logo" 
+          className="App-logo" 
+        />
+        <h1>Redi React Fall 2020 Team</h1>
+      </header>
+      <main>
+        <table className="members-table">
+          <thead>
+            <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {team.map((member) => (
+              <tr className="member-row" key={member.name}>
+                <td className="avatar-cell">
+                  <img src={member.image} alt={member.name} />
+                </td>
+                <td className="name-cell">{member.name}</td>
+                <td className="role-cell">{member.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </>
+  );
 }
